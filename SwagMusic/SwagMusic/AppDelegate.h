e0a2c73f829f55a521f0a9a4e8ba34c77a7c5374
st7michael <st7michael@163.com>
2017-11-09 10:59:21 +0800

    Set up SwagMusic

---
+++ SwagMusic/SwagMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwagMusic
+//
+//  Created by Yiqiao on 2017/11/9.
+//  Copyright © 2017年 YiqiaoSwag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwagMusic/SwagMusic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwagMusic
+//
+//  Created by Yiqiao on 2017/11/9.
+//  Copyright © 2017年 YiqiaoSwag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
